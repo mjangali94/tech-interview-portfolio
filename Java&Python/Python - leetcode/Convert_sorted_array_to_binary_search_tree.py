7dee87ce5fd93e48a81bc72bde7cd6daf6fa9c5a
BATCHEXECUTIONBENCHMARK@GMAIL.COM <60479217+mjangali94@users.noreply.github.com>
2025-11-03 18:46:17 -0500

    batch for Nov 3

---
+++ Java&Python/Python - leetcode/Convert_sorted_array_to_binary_search_tree.py
@@ -0,0 +1,25 @@
+# Problem: Convert Sorted Array to Binary Search Tree
+# LeetCode URL: https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+# Difficulty: Easy
+# Description: Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+
+        if not nums:
+            return None
+
+        mid = len(nums) // 2
+        root = TreeNode(nums[mid])
+
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+
+        return root
+        
---
+++ Java&Python/Python - leetcode/Maximum_depth_of_binary_tree.py
@@ -0,0 +1,26 @@
+# Problem: Maximum Depth of Binary Tree
+# LeetCode URL: https://leetcode.com/problems/maximum-depth-of-binary-tree
+# Difficulty: Easy
+# Description: Given the root of a binary tree, return its maximum depth. A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        left_max_depth= self.maxDepth(root.left)
+        right_max_depth= self.maxDepth(root.right)
+        if left_max_depth >= right_max_depth:
+            return left_max_depth + 1
+        else:
+            return right_max_depth +1
+
+        
+
+
